62481f383f602a651f9e8fe7a3ab555abde659bb
vutrinh <hoangvu.13kth1@gmail.com>
2024-03-12 15:30:06 +0700

    Registration and login part.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/signup', to: "registrations#new"
+  post '/signup', to: "registrations#create"
+
+  get 'login', to: "sessions#new"
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
+
 end
